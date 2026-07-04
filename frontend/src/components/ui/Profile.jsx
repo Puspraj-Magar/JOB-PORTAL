f7fd931237b0ea30a4cbf479629b87cf15541270
Puspraj Magar <pusprajmagar@gmail.com>
2026-07-05 00:11:07 +0530

    feat: improve job portal UI and authentication

--- frontend/src/components/ui/Profile.jsx
+++ frontend/src/components/ui/Profile.jsx
@@ -3,12 +3,18 @@ import Navbar from "../shared/Navbar";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "./button";
 import { Contact, Mail, Pen } from "lucide-react";
+import { Badge } from "./badge";
+import { Label } from "./label";
+import AppliedJobTable from "../AppliedJobTable";
 
+const skills = ["Java", "Python", "JavaScript", "React.js", "Node.js"];
 const Profile = () => {
+
+  const isResume = true;
   return (
     <div>
       <Navbar />
-      <div className="max-w-7xl mx-auto bg-white border border-gray-200 rounded-2xl my-5 p-8">
+      <div className="max-w-4xl mx-auto bg-white border border-gray-200 rounded-2xl my-5 p-8">
         <div className="flex justify-between">
           <div className="flex items-center gap-4">
             <Avatar className="h-24 w-24">
@@ -25,18 +31,37 @@ const Profile = () => {
           <Button className="text-right" variant="outline">
             <Pen />
           </Button>
+        </div>
+        <div>
+          <div className="flex items-center gap-3 my-2">
+            <Mail />
+            <span>kaji@gmail.com</span>
           </div>
-          <div>
-            <div className="flex items-center gap-3 my-2">
-              <Mail />
-              <span>kaji@gmail.com</span>
-            </div>
-            <div className="flex items-center gap-3 my-2">
-              <Contact />
-              <span>8080088088</span>
-            </div>
+          <div className="flex items-center gap-3 my-2">
+            <Contact />
+            <span>8080088088</span>
           </div>
-        
+        </div>
+        <div className="my-5">
+          <h1>Skills</h1>
+          <div className="flex items-center gap-1">
+            {skills.length != 0 ? (
+              skills.map((items, index) => <Badge key={index}>{items}</Badge>)
+            ) : (
+              <span>NA</span>
+            )}
+          </div>
+        </div>
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label className={'text-md font-bold'}>Resume</Label>
+          {
+            isResume ? <a target="blank" href="https://www.youtube.com/@KshitijRana-rb6rj" className="text-blue-500 w-full hover:underline cursor-pointer"> Kaji Magar </a> : <span>NA</span>
+          }
+        </div>
+      </div>
+      <div className="max-w-4xl mx-auto bg-white rounded-2xl">
+        <h1>Applied Job</h1>
+        <AppliedJobTable />
       </div>
     </div>
   );
